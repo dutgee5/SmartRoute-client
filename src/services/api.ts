b020d7b39966a85ab129ab5744e7798a564b034c
theDone <done.muzaffer5@gmail.com>
2025-11-25 11:16:48 +0300

    Initalize expo project and folder project structure.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
-// src/services/api.ts
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-// ⚠️ EĞER ANDROID EMULATOR KULLANIYORSAN: 'http://10.0.2.2:5118' (Portu kendi backend portunla değiştir)
-// ⚠️ EĞER FİZİKSEL TELEFON KULLANIYORSAN: Bilgisayarının IP adresi (Örn: 'http://192.168.1.35:5118')
 const API_URL = 'http://192.168.1.157:7181';
 const api = axios.create({
     baseURL: API_URL,
@@ -12,7 +9,6 @@ const api = axios.create({
     },
 });
 
-// Her istekten önce otomatik Token ekleme
 api.interceptors.request.use(async (config) => {
     try {
         const token = await SecureStore.getItemAsync('userToken');
